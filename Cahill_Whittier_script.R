2ef22ee3b57398e722d31b48014e2cb83009943a
Cahill Whittier <cahillwhittier@cahills-mbp.bc.edu>
2023-09-10 16:43:18 -0400

    Cahill Whittier final script

--- Cahill_Whittier_script.R
+++ Cahill_Whittier_script.R
@@ -5,18 +5,14 @@ library("tidyverse")
 #Set working directory
 setwd("/Users/cahillwhittier/test")
 
-##Downloads scales.csv and create dat variable
-library(readr)
+##Download scales.csv and create dat variable
 dat <- read_csv("~/Downloads/scales.csv")
 
 ##class of each column in the data set
 sapply(dat,class)
 
-##The names of the species in the data set, and how many different species there are
-dat$species <- as.factor(dat$species)
-species <- levels(dat$species)
-species
-length(species)
+##Dimensions of the data set
+dim(dat)
 
 ##How many scales punctured per species
 species.n<- dat %>%
@@ -32,13 +28,6 @@ dat %>%
 
 
 ##plotting mean puncture force for each quadrant in each species
-for(i in species){
-  p <- dat %>%
-    filter(species==i)%>%
-    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
-  print(p)
-}
-
 ##Create PDF file containing all the figures
 pdf("Cahill_Whittier_species_quadrant.pdf")
 for(i in species){
@@ -49,4 +38,3 @@ for(i in species){
 }
 dev.off()
 
-list.files(pattern=".pdf")
