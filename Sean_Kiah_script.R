ad4caaf7abb9fc70d89fe1726331bf516b0db72e
kiahs12 <kiahs@bc.edu>
2023-09-10 13:02:34 -0400

    Group 9

--- Sean_Kiah_script.R
+++ Sean_Kiah_script.R
@@ -1,70 +1,29 @@
 library(ggplot2)
 library(tidyverse)
 
-library(ggplot2)
-
 setwd("/Users/seankiah/Desktop/Scales/Group-9")
+
+#1) Line of code with dat variable containing the scales dataset
 dat<-read.csv("scales.csv")
 
+#3) Line of code that shows the dimensions of the dataset
 dim(dat)
-head(dat)
 
-class(dat$N)
-class(dat$quadrant)
-class(dat$species)
-class(dat$specimen)
-class(dat[,1])
-class(dat[,2])
-class(dat[,4])
-
-mean(dat$N)
-mean(dat$quadant)
-
+#2) Line of code that reports the class of each column in dataset
 sapply(dat,class)
-?sapply(dat,class)
 
-dat$species<-as.factor(dat$species)
-species<-levels(dat$species)
-species
-
-length(species)
-
-dat$species==species[1]
-
-dat$species[dat$species==species[1]]
-
-A.rup<-length(dat$species[dat$species==species[1]])
-L.gib<-length(dat$species[dat$species==species[2]])
-L.mac<-length(dat$species[dat$species==species[3]])
-M.sal<-length(dat$species[dat$species==species[4]])
-M.sax<-length(dat$species[dat$species==species[5]])
-P.fla<-length(dat$species[dat$species==species[6]])
-species.obs<-data.frame(sp=species,n=c(A.rup,L.gib,L.mac,M.sal,M.sax,P.fla))
-species.obs
-
-dat%>%
-  group_by(species) %>%
-  summarise(n=n())
-
+#4) Code that shows the the number of scales punctured for each species
 species.n<-dat%>%
   group_by(species) %>%
   summarise(n=n())
 species.n
-
+#5) Code that shows how many specimens are sampled for each species
 dat %>%
   count(species,specimen) %>%
   print() %>%
   count(species,name = "n.specimens")
 
-for(i in 1:10) print(i)
-
-for(i in species) {
-  p <- dat %>%
-    filter(species==i) %>%
-    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
-  print(p)
-}
-
+#6)Code that produces a PDF containing a boxplot of puncture force vs quadrant
 pdf("Sean_Kiah_species_quadrant.pdf")
 for(i in species) {
   p <- dat %>%
@@ -72,7 +31,3 @@ for(i in species) {
     ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
   print(p)
 }
-dev.off()
-
-list.files(pattern=".pdf")
-
